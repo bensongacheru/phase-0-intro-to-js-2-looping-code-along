a3bdc29004755e7945a76155a73012005716e77a
benson <mwangibenson923@gmail.com>
2024-07-20 01:28:10 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function writeCards(namesArray,eventName) {
+    let messages = []
+    for (let i = 0; i < namesArray.length; i++) {
+        
+        messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages;
+}
+let names = ["Guadalupe", "Ollie", "Aki"]
+let eventName = 'suprise'
+let cards = writeCards(names,eventName)
+console.log(cards);
+
+function countDown(startingNurmbers) {
+    let currentNumber = startingNurmbers
+
+    while ( currentNumber>=0) {
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
+countDown;
